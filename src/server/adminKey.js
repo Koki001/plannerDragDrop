be3502867f5550817a8a2bb7b66e34f3d566f0d4
kokivasileski <thekoki001@gmail.com>
2022-12-08 01:08:10 -0500

    removed key

--- src/server/adminKey.js
+++ src/server/adminKey.js
@@ -1,10 +1,1 @@
-// const admin = require("firebase-admin");
-// const serviceAccount = require("./daily-planner-5f81a-firebase-adminsdk-5knuz-3d06a66bb7.json");
 
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://daily-planner-5f81a-default-rtdb.firebaseio.com",
-// });
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
